796c9f45d86a9c89628711b3eddc5d2c1dcd89ea
= <garvitgalgat@gmail.com>
2020-10-27 15:03:04 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, url_for, redirect, session
+import os
+from authlib.integrations.flask_client import OAuth
+
+app = Flask(__name__)
+
+
+app.secret_key = "my secret key"
+
+# OAuth Config
+oauth = OAuth(app)
+google = oauth.register(
+    name="google",
+    client_id="76138501739-k1hfqpb9qbtgo6kndji79442bi9ar32s.apps.googleusercontent.com",
+    client_secret="b7FXvKAuxcyXHX4n34u3vN-U",
+    access_token_url="https://accounts.google.com/o/oauth2/token",
+    access_token_params=None,
+    authorize_url="https://accounts.google.com/o/oauth2/auth",
+    authorize_params=None,
+    api_base_url="https://www.googleapis.com/oauth2/v1/",
+    userinfo_endpoint="https://openidconnect.googleapis.com/v1/userinfo",  # This is only needed if using openId to fetch user info
+    client_kwargs={"scope": "openid email profile"},
+)
+
+
+@app.route("/")
+def hello_world():
+    email = dict(session).get("email", None)
+    return f"Hello {email}"
+
+
+@app.route("/login")
+def login():
+    google = oauth.create_client("google")
+    redirect_uri = url_for("authorize", _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route("/authorize")
+def authorize():
+    google = oauth.create_client("google")
+    token = google.authorize_access_token()
+    resp = google.get("userinfo")
+    user_info = resp.json()
+    # do something with the token and profile
+    session["email"] = user_info["email"]
+    print(user_info)
+    return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect("/")
